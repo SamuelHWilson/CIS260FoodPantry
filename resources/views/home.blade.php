541d4b3b5f6a2175c052c7bf5182f9fcc10f43af
Unknown <swilsondev@outlook.com>
2018-10-13 16:02:48 -0500

    Setting up bare-bones project.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main-layout')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>This is the content</h1>
+@endsection
---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>@yield('title')</title>
+    </head>
+</html>
+<body>
+    <div class='container'>
+        <div class='content'>
+            @yield('content')
+        </div>
+    </div>
+</body>
