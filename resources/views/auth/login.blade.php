66f7769a513ac0e6cb3ca0aecaac42f1db3ab1d3
Michael Straub <ms0848783@otc.edu>
2018-11-15 21:55:53 -0600

    Git change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,29 +23,18 @@ LAST UPDATE: 11/05/2018-->
 
       <!--Input Elements-->
       <div class="Index_login">
-<<<<<<< HEAD
-        <label for="uname"><b>Account: </b></label><br>
-        <input type="radio" name="username" id="read_write" value="read/write"> Read/Write<br>
-        <input type="radio" name="username" id="ready_only" value="read_only"> Read Only<br>
-        <br><br>
-        <label for="psw"><b>Password: </b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-        <br><br>
-        <!--Submit Button-->
-        <button type="submit">Login</button>
-=======
         <form method="POST" action="/login">
           @csrf
-          <label for="name"><b>Username: </b></label>
-          <input type="text" placeholder="Enter Username" name="name" required>
-          <br><br>
+        <label for="uname"><b>Account: </b></label><br>
+        <input type="radio" name="name" id="read_write" value="edit"> Read/Write<br>
+        <input type="radio" name="name" id="ready_only" value="view"> Read Only<br>
+        <br><br>
           <label for="password"><b>Password: </b></label>
           <input type="password" placeholder="Enter Password" name="password" required>
           <br><br>
           <!--Submit Button-->
           <input type="submit" value='Login'>
         </form>
->>>>>>> master
       </div>
     </div>
   </div>
