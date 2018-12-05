17815d923370257ce0c63185dd8bf2d9a44b7f8e
Kyle Turner <kt0857159@otc.edu>
2018-12-04 23:10:01 -0600

    Added printable reports pages
    Added two pages that are plain and formatted to print appropriately. Report for daily schedule and report for frequent no-shows

---
+++ resources/views/crud/daily-schedule-report.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang = "en">
+	<head>
+		<title>REPORTS PAGE</title>	
+		<link href="{{ asset('css/style.css') }}" rel='stylesheet' />
+	</head>
+	<body>
+		<div class="page">
+			<h2 style="text-align: center;">APPOINTMENTS FOR 12-20-2018</h2>
+			<table style = "width: 100%">
+				<tr>
+					<th>APPOINTMENT TIME</th>
+					<th>CLIENT NAME</th>
+					<th>PHONE NUMBER</th>
+					<th>SENIOR BOX</th>
+				</tr>
+				<tr>
+					<td>08:00 AM</td>
+					<td>Richard Test</td>
+					<td>4175550000</td>
+					<td>NO</td>
+				</tr>
+				<tr>
+					<td>08:00 AM</td>
+					<td>Richard Test</td>
+					<td>4175550000</td>
+					<td>NO</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
---
+++ resources/views/crud/no-show-report.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang = "en">
+	<head>
+		<title>REPORTS PAGE</title>	
+		<link href="{{ asset('css/style.css') }}" rel='stylesheet' />
+	</head>
+	<body>
+		<div class="page">
+			<h2 style="text-align: center;">FREQUENT NO-SHOWS</h2>
+			<table style = "width: 100%">
+				<tr>
+					<th>CLIENT NAME</th>
+					<th>PHONE NUMBER</th>
+					<th>SENIOR BOX</th>
+				</tr>
+				<tr>
+					<td>Richard Test</td>
+					<td>4175550000</td>
+					<td>NO</td>
+				</tr>
+				<tr>
+					<td>Richard Test</td>
+					<td>4175550000</td>
+					<td>NO</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
