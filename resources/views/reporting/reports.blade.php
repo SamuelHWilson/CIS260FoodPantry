7b3016d8187b1443af63271646beb318c5840295
Unknown <swilsondev@outlook.com>
2018-12-05 20:33:04 -0600

    Modifying reports page.

--- resources/views/reporting/reports.blade.php
+++ resources/views/reporting/reports.blade.php
@@ -12,25 +12,23 @@
     </head>
 	<body class = "Index_body">
 		<div class = "Index_mainDiv">
-			<div class = "Index_container">
+			<div class = "Index_container clearfix">
 			
 				<div>
 					<h1 class="defaultconfig_h1"><font face="Helvetica">REPORTS</font></h1>
 				</div>
 				
 				<div class = "left">
-					<h2>SELECT HOW TO GENERATE NO-SHOW REPORT</h2>
+					<h2>FREQUENT NO-CALL NO-SHOW REPORT</h2>
 					<div style = "text-align: center;">
 						<button class = "button" onclick='location.href="/reporting/no-shows"'>View Frequent No-Shows</button>
-						<button class = "button" onclick='location.href="/"'>Print Frequent No-Shows</button>
 					</div>
 				</div>
-				<div class = "clear">
-					<h2>SELECT DATE AND HOW TO GENERATE DAILY SCHEDULE REPORT</h2>								
+				<div class = "right">
+					<h2>CLIENTS BY DAY REPORT</h2>								
 					<div style = "text-align: center;">
 						<b>Date: </b><input id="date" type="text" />
 						<button class = "button" onclick='location.href="/reporting/daily/" + document.getElementById("date").value'>View Daily Schedule</button>
-						<button class = "button" onclick='location.href="/reporting/daily/"'>Print Daily Schedule</button>
 					</div>
 				</div>
 			</div>
