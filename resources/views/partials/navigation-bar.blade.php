85fc6e5d539c5b86ba6db333ff0df8a84fedcc7b
Unknown <swilsondev@outlook.com>
2019-08-18 20:02:59 -0500

    UI tweak

--- resources/views/partials/navigation-bar.blade.php
+++ resources/views/partials/navigation-bar.blade.php
@@ -7,5 +7,6 @@
     <li class="navItem"><a class="navBar" href="/reporting/reports">Reports</a></li>
     <li class="navItem"><a class="navBar" href="/hours/view-hours">Hours and Availability</a></li>
     <li class="navItem"><a class="navBar" href="/password/change">Change Password</a></li>
+    <li class="navItem"><a class="navBar" href="/appointments/create-bulk">Bulk Create</a></li>
     <li class="navItem" style="float:right" onclick='document.forms.namedItem("logout").submit()'><a class="navBar">Lock Page</a></li>
 </ul>
